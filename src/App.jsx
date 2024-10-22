8a66018cb610044edd6fa3dc3b037be9c947a4b4
Jhovan <perjhoal@gmail.com>
2024-10-22 00:18:06 -0600

    Estructura de metas

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import './App.css'
 import Encabezado from './componentes/compartidos/Encabezado'
 import Pie from './componentes/compartidos/Pie'
 import Principal from './componentes/compartidos/Principal'
+import Meta from './componentes/lista/Meta'
 
 function App() {
 
   return (
     <>
       <Encabezado></Encabezado>
-      <Principal></Principal>
+      <Principal> <Meta></Meta> </Principal>
       <Pie></Pie>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -15,6 +16,6 @@ export default {
        },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-neumorphism')],
 }
 
